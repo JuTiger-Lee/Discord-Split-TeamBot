6d8de4b8a2424fdd67c05f0681d6b1680e092955
Jeffey <ref6318@gmail.com>
2022-09-12 21:19:40 +0900

    [FIX]: 웹서버 코드 제거

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-const http = require("http");
 const dotenv = require("dotenv");
 const app = require("./app");
 dotenv.config();
 
 (function () {
   app.bootstrap().login(process.env.SPLIT_TEAM_TOKEN);
-  http
-    .createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      return res.end("Discord Split Team Bot Server");
-    })
-    .listen(process.env.PORT);
 })();
